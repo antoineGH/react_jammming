755dbe171cd2f6550bc023f58e0485df8afaeb05
Templars <antoine.ratat@gmail.com>
2021-04-14 14:51:05 +0800

    Obtain a Spotify Access Token

---
+++ src/util/Spotify.js
@@ -0,0 +1,28 @@
+import config from '../config'
+const clientId = config.clientId
+// const clientSecret = config.clientSecret
+const redirectUri = 'http://localhost:3000'
+
+let accessToken = ''
+const Spotify = {
+	getAccessToken() {
+		if (accessToken) return accessToken
+
+		const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+		const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+		if (accessTokenMatch && expiresInMatch) {
+			accessToken = accessTokenMatch[1]
+			const expiresIn = Number(expiresInMatch[1])
+
+			window.setTimeout(() => (accessToken = ''), expiresIn * 1000)
+			window.history.pushState('Access Token', null, '/')
+			return accessToken
+		} else {
+			const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
+			window.location = accessURL
+		}
+	},
+}
+
+export default Spotify
