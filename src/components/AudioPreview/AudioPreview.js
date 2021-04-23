83aafd173b800f50ff47ffa62c0c35af45117030
Templars <antoine.ratat@gmail.com>
2021-04-24 00:28:50 +0800

    media load intersection observer api

--- src/components/AudioPreview/AudioPreview.js
+++ src/components/AudioPreview/AudioPreview.js
@@ -1,15 +1,32 @@
-import React, { Component } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './audioPreview.css'
 
-export default class AudioPreview extends Component {
-	render() {
-		const { preview } = this.props
-		return (
-			<div className='audioPlayer'>
-				<audio controls>
-					<source src={preview} type='audio/mp3'></source>
-				</audio>
-			</div>
+export default function AudioPreview(props) {
+	const { preview } = props
+	const thisPreview = useRef()
+
+	useEffect(() => {
+		console.log('useEffect')
+		console.log(thisPreview.currentSrc)
+		let myObserver = new IntersectionObserver(
+			(entries) =>
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						console.log('interect audio')
+						thisPreview.current.src = preview
+						myObserver = myObserver.disconnect()
+					}
+				}),
+			{ rootMargin: '0px 0px 200px 0px' }
 		)
-	}
+		myObserver.observe(thisPreview.current)
+	}, [preview])
+
+	return (
+		<div className='audioPlayer'>
+			<audio controls ref={thisPreview}>
+				<source src={''} type='audio/mp3'></source>
+			</audio>
+		</div>
+	)
 }
